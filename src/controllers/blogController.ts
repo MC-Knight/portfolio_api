aca73e895bd0563fd8d6ff519cb5b72cc66737f6
Mc-Knight <tetobobo1@gmail.com>
2024-03-12 17:49:26 +0200

    return type of a promise on endpoints

--- src/controllers/blogController.ts
+++ src/controllers/blogController.ts
@@ -2,12 +2,14 @@ import { type Request, type Response } from "express";
 import { Blog, validate } from "../models/blog";
 
 class BlogController {
-  static async createBlog(req: Request, res: Response): Promise<void> {
+  static async createBlog(
+    req: Request,
+    res: Response
+  ): Promise<undefined | Response<any, Record<string, any>>> {
     try {
       const { error } = validate(req.body);
       if (error !== undefined) {
-        res.status(400).json({ error: error?.details[0].message });
-        return;
+        return res.status(400).json({ error: error?.details[0].message });
       }
 
       const blog = new Blog({
@@ -23,16 +25,18 @@ class BlogController {
     }
   }
 
-  static async getSingleBlog(req: Request, res: Response): Promise<void> {
+  static async getSingleBlog(
+    req: Request,
+    res: Response
+  ): Promise<undefined | Response<any, Record<string, any>>> {
     try {
       const blogId = req.params.id;
       const blog = await Blog.findById(blogId);
 
       if (blog == null) {
-        res
+        return res
           .status(404)
           .json({ error: "blog with the given ID was not found." });
-        return;
       }
 
       res.status(200).json({ blog });
@@ -41,20 +45,25 @@ class BlogController {
     }
   }
 
-  static async getAllBlogs(req: Request, res: Response): Promise<void> {
+  static async getAllBlogs(
+    req: Request,
+    res: Response
+  ): Promise<undefined | Response<any, Record<string, any>>> {
     try {
       const blogs = await Blog.find();
       res.status(200).json({ blogs });
     } catch (error) {
-      res.status(400).json({ error: "Something went wrong" });
+      return res.status(400).json({ error: "Something went wrong" });
     }
   }
 
-  static async editBlogs(req: Request, res: Response): Promise<void> {
+  static async editBlogs(
+    req: Request,
+    res: Response
+  ): Promise<undefined | Response<any, Record<string, any>>> {
     const { error } = validate(req.body);
     if (error !== undefined) {
-      res.status(400).json({ error: error?.details[0].message });
-      return;
+      return res.status(400).json({ error: error?.details[0].message });
     }
 
     const blog = await Blog.findByIdAndUpdate(req.params.id, {
@@ -70,12 +79,16 @@ class BlogController {
     res.status(200).json({ message: "blog edited successfully" });
   }
 
-  static async deleteBlogs(req: Request, res: Response): Promise<void> {
+  static async deleteBlogs(
+    req: Request,
+    res: Response
+  ): Promise<undefined | Response<any, Record<string, any>>> {
     const blog = await Blog.findByIdAndDelete(req.params.id);
 
     if (blog == null) {
-      res.status(404).json({ error: "blog with the given ID was not found." });
-      return;
+      return res
+        .status(404)
+        .json({ error: "blog with the given ID was not found." });
     }
 
     res.status(200).json({ message: "blog deleted successfully" });
