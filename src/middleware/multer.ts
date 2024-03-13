0fb58e71b1329bfc1cc486830950c33045d0d8f4
Mc-Knight <tetobobo1@gmail.com>
2024-03-13 16:25:08 +0200

    all blogs with there comments

---
+++ src/middleware/multer.ts
@@ -0,0 +1,23 @@
+import multer from "multer";
+import { type Request } from "express";
+
+const storage = multer.diskStorage({
+  destination: function (
+    req: Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, destination: string) => void
+  ) {
+    cb(null, "./uploads");
+  },
+  filename: function (
+    req: Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, filename: string) => void
+  ) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
--- src/routes/commentRouter.ts
+++ src/routes/commentRouter.ts
@@ -4,5 +4,6 @@ import CommentController from "../controllers/commentController";
 const routerComment = express();
 
 routerComment.post("/create", CommentController.createComment);
+routerComment.delete("/delete/:id", CommentController.deleteComments);
 
 export default routerComment;
