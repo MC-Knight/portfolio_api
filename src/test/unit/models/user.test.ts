8add1ae499983525b7f7404cf5657afa4364545d
Mc-Knight <tetobobo1@gmail.com>
2024-03-18 14:21:13 +0200

    unite test setup

---
+++ src/test/unit/models/user.test.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+import { User } from "../../../models/user";
+import jwt, { Secret, JwtPayload } from "jsonwebtoken";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { describe, expect, it } from "@jest/globals";
+
+dotenv.config();
+
+describe("user.generateAuthToken", () => {
+  it("should return a valid JWT", () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true,
+    };
+    const user = new User(payload);
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_ACCESS_KEY as Secret
+    ) as JwtPayload;
+    expect(decoded).toMatchObject(payload);
+  });
+});
